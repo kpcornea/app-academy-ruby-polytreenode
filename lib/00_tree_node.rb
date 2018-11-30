25da63b3f7012ad733c3a92d6a01a5331436ea5e
Kevin Cornea <kpcornea@gmail.com>
2018-11-30 14:09:36 -0800

    Complete Phase 1 and Phase 2

--- lib/00_tree_node.rb
+++ lib/00_tree_node.rb
@@ -1,3 +1,31 @@
 class PolyTreeNode
 
+  def initialize(value)
+    @value = value
+    @parent = nil
+    @children = []
+  end
+
+  def parent
+    @parent
+  end
+
+  def children
+    @children
+  end
+
+  def value
+    @value
+  end
+
+  def parent=(node)
+    @parent.children.delete(self) unless @parent == nil
+    @parent = node
+    @parent.children << self unless node == nil
+  end
+
+
 end
+
+
+# lib/00_tree_node.rb
